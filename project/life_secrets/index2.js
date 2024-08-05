a91da93a6df41647ffc5f7b50d59b63546588f25
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2024-08-05 22:44:50 +0530

    Create index2.js

---
+++ project/life_secrets/index2.js
@@ -0,0 +1,53 @@
+//To see how the final website should work, run "node solution.js".
+//Make sure you have installed all the dependencies with "npm i".
+//The password is ILoveProgramming
+import express from "express";
+import bodyParser from "body-parser";
+import session from "express-session";
+
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+
+const app = express();
+const port = 3000;
+
+// var verifiedUser = false;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(session({
+    secret: 'your-secret-key',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}));
+
+function verifyPassword(req, res, next) {
+    var pass = req.body["password"];
+    if(pass === "abc123") {
+        req.session.verifiedUser = true;
+    }
+    next();
+}
+
+app.use(verifyPassword);
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post('/check', (req, res) => {
+    if(req.session.verifiedUser) {
+        res.sendFile(__dirname + "/public/secret.html");
+    }
+    else {
+        res.redirect("/");
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
